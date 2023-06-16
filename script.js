6d6f99e3f433b2f70daf95bbd1229b8b74a061dd
Subham Sahu <115224258+subham13-02@users.noreply.github.com>
2023-06-17 01:10:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 function letterCombinations(input_digit) {
-  //Complete the function
+  let table={
+	  2:"abc" ,
+	  3:"def" ,
+	  4:"ghi" ,
+	  5:"jkl" ,
+	  6:'mno',
+	  7:"pqrs" ,
+	  8:"tuv" ,
+	  9:"wxyz"
+  }
+	let sol=[];
+
+	function solve(index,String){
+		if(input_digit.lenth<=index){
+			sol.push(String);
+			return;
+		}
+		let char=input_digit[index];
+		for(let i=0;i<table[char].length;i++){
+			solve(index+1,String+table[char][i])
+		}
+	}
+	solve(0,"");
 }
 
 module.exports = letterCombinations;
