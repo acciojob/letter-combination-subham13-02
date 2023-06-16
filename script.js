5ebaba46f992a4084962226a0f8cc15d11305d5c
Subham Sahu <115224258+subham13-02@users.noreply.github.com>
2023-06-17 01:16:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function letterCombinations(input_digit) {
   let table={
-	  2:"abc" ,
-	  3:"def" ,
-	  4:"ghi" ,
-	  5:"jkl" ,
-	  6:'mno',
-	  7:"pqrs" ,
-	  8:"tuv" ,
-	  9:"wxyz"
+	  "2":"abc" ,
+	  "3":"def" ,
+	  "4":"ghi" ,
+	 " 5":"jkl" ,
+	 " 6":'mno',
+	  "7":"pqrs" ,
+	  "8":"tuv" ,
+	  "9":"wxyz"
   }
 	let sol=[];
 
